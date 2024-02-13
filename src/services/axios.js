d41caeb674c8d7974148ea7d20ae3888d8b1593b
RayG-XD <mail.pratikpol@gmail.com>
2024-02-13 22:18:30 +0530

    made changes to env variables

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,19 @@
+import { APP_ENVIRONMENT } from '@root/App';
 import axios from 'axios';
 
-const BASE_URL = `${process.env.REACT_APP_BASE_ENDPOINT}/api/v1`;
+export let BASE_ENDPOINT = '';
+
+if (APP_ENVIRONMENT === 'local') {
+  BASE_ENDPOINT = 'http://localhost:5000';
+} else if (APP_ENVIRONMENT === 'development') {
+  BASE_ENDPOINT = 'https://api.dev.nutanex.co';
+} else if (APP_ENVIRONMENT === 'staging') {
+  BASE_ENDPOINT = 'https://api.stg.nutanex.co';
+} else {
+  BASE_ENDPOINT = 'https://api.nutanex.co';
+}
+
+const BASE_URL = `${BASE_ENDPOINT}/api/v1`;
 
 export default axios.create({
   baseURL: BASE_URL,
